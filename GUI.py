33376a3785eb889ea46d9633211e39dc300a8eef
iSayU <huy.nguyenvinh@hcmut.edu.vn>
2024-08-09 15:53:57 +0700

    fix: fix folder directory picker

--- GUI.py
+++ GUI.py
@@ -1,5 +1,5 @@
 from config import directory, prefix, include_folder_name, space_al, is_lower_cap
-from change_name import change_filenames, change_folder_name
+from change_name import change_filenames, change_folder_name, new_filename
 from tkinter import *
 from tkinter import filedialog
 
@@ -19,11 +19,12 @@ def change_folder_name_cmd():
         global folder_directory_entry
         new_dir = change_folder_name(directory=directory,folder_new_name=new_folder_name)
         folder_directory_entry.delete(0, END)
-        folder_directory_entry.insert(new_dir)
+        folder_directory_entry.insert(0, new_dir)
     except Exception as e:
         create_error_popup(error=e)
 
 def browse_folder_cmd():
+    #TODO: Try catch
     global folder_name_browse
     global folder_directory_entry
     folder_name_browse = filedialog.askdirectory(
@@ -33,7 +34,7 @@ def browse_folder_cmd():
     folder_directory_entry.insert(folder_name_browse)
 
 main = Tk()
-main.geometry("750x250")
+main.geometry("750x750")
 
 #Lower cap tick box
 cap_tick_box = BooleanVar()
@@ -67,18 +68,18 @@ space_al_text_box.grid(row=4,column=1,columnspan=2, sticky=W)
 browse_folder_label = Label(main, text="Choose Folder Directory")
 browse_folder_label.grid(row=5, column= 0, columnspan=2, sticky=W)
 
-folder_directory_entry = Entry(main, textvariable=folder_name_browse)
-folder_directory_entry.grid(row=6,column=0,columnspan=2, sticky=W)
+folder_directory_entry = Entry(main, textvariable=folder_name_browse, width=60)
+folder_directory_entry.grid(row=6,column=0,columnspan=4, sticky=W)
 
 folder_browse_btn = Button(main, text="Browse", command=browse_folder_cmd)
-folder_browse_btn.grid(row=6, column=2, sticky=W)
+folder_browse_btn.grid(row=6, column=5, sticky=W, padx= 10)
 
 #Rename Folder
 rename_folder_label = Label(main, text="Rename folder")
 rename_folder_label.grid(row=7, column= 0, columnspan=2, sticky=W)
 
-rename_folder_entry = Entry(main, textvariable=new_folder_name)
-rename_folder_entry.grid(row=8,column=0,columnspan=2, sticky=W)
+rename_folder_entry = Entry(main, textvariable=new_folder_name, width=40)
+rename_folder_entry.grid(row=8,column=0,columnspan=4, sticky=W)
 
 rename_folder_btn = Button(main, text="Rename", command=change_folder_name_cmd)
 rename_folder_btn.grid(row=8, column=2, sticky=W)
